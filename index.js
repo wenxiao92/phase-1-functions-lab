dbbbb94f66d7bac9b847cb8095b926f3d968c863
Wen Xiao <wennstyle@gmail.com>
2021-09-09 21:57:02 -0400

    commit tree d0a4d0e5795b3936f7a3803747e637c81be7c726

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+let someValue;
+
+function distanceFromHqInBlocks(someValue) {
+    if(someValue > 42) {
+        return someValue - 42;
+    }
+        else {return 42 - someValue;
+        }
+}
+
+function distanceFromHqInFeet(someValueFt) {
+    if(someValueFt > 42) {
+        return (someValueFt - 42) * 264;
+    }
+        else {return (42 - someValueFt) * 264;
+        }
+}
+
+function distanceTravelledInFeet(begin, end) {
+    if(begin > end) {
+        return (begin - end) * 264;
+    } 
+        else {return (end - begin) * 264; 
+        }
+}
+
+function calculatesFarePrice(start, destination) {
+    if(Math.abs(start-destination) * 264 < 400) {
+        return 0;
+    }
+    
+    else if(Math.abs(start-destination) * 264 > 400 && Math.abs(start-destination) * 264 < 2000) {
+        return ((Math.abs(start-destination) * 264) - 400) * .02;
+    }
+    else if(Math.abs(start-destination)* 264 > 2000 && Math.abs(start-destination) * 264 < 2500) {
+        return 25;
+        }
+    else if(Math.abs(start-destination) * 264 > 2500)
+        {return 'cannot travel that far';
+    }
+
+}
